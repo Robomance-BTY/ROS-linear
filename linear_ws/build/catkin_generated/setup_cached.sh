ea2a3b27f8a79c66b29a9c01cbab7b1adc052afb
didxotlrWkd <xmflr28@gmail.com>
2024-04-03 18:36:03 +0000

    2024-04-04

---
+++ linear_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/taeshik/ROS-Linear/linear_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/taeshik/ROS-Linear/linear_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/taeshik/ROS-Linear/linear_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/taeshik/ROS-Linear/linear_ws/src:$ROS_PACKAGE_PATH"
